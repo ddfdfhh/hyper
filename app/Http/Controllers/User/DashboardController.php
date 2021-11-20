d5cc44b3203ae592d37a81b94798952ec8aa4999
ddfdfhh <shashi801@gmail.com>
2021-11-20 11:41:45 +0530

    dd

--- app/Http/Controllers/User/DashboardController.php
+++ app/Http/Controllers/User/DashboardController.php
@@ -16,7 +16,7 @@ class DashboardController extends Controller{
     //index
     public function index(Request $request){
         
-       $my_posts=Hpyerzone::with('owner','comments')->withCount('all_comments')->where('user_id',auth()->id())->latest()->get();
+       $my_posts=Hyperzone::with('owner','comments')->withCount('all_comments')->where('user_id',auth()->id())->latest()->get();
         $following_count1=Follower::where('user_id',auth()->id())->count();
         $followers_count1=Follower::where('following_id',auth()->id())->count();
          $news = News::whereStatus('Active')->orderby('id', 'DESC')->get();
